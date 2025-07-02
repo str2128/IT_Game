f2986c39ff599992f05fc6a097d09c2dce458d69
Stephanie Angueira <StephanieCAngueira@gmail.com>
2025-07-02 18:46:39 -0400

    Fixed formatting, added red dot and email

--- src/components/Desktop1.js
+++ src/components/Desktop1.js
@@ -1,59 +1,124 @@
+import React, { useState } from 'react';
+
 function Desktop() {
+  const [showEmail, setShowEmail] = useState(false);
+  const [hasNewEmail, setHasNewEmail] = useState(true);
+
   return (
     <div style={styles.desktop}>
 
-        {/* Row 1 */}
+      {/* Row 1 */}
       <div style={styles.iconRow}>
         {/* Email */}
-        <div style={styles.icon} onClick={() => console.log('Email clicked')}>
-          <img src="/assets/icons/Email.png" alt="Email" style={styles.iconImage} />
+        <div
+          style={styles.icon}
+          onClick={() => {
+            setShowEmail(true);
+            setHasNewEmail(false);
+          }}
+        >
+          <div style={{ position: 'relative' }}>
+            <img
+              src="/assets/icons/Email.png"
+              alt="Email"
+              style={styles.iconImage}
+            />
+            {hasNewEmail && <div style={styles.redDot} />}
+          </div>
           <div style={styles.iconLabel}>Email</div>
         </div>
+
         {/* Trash */}
         <div style={styles.icon} onClick={() => console.log('Trash clicked')}>
-          <img src="/assets/icons/Trash.png" alt="Trash" style={styles.iconImage} />
+          <img
+            src="/assets/icons/Trash.png"
+            alt="Trash"
+            style={styles.iconImage}
+          />
           <div style={styles.iconLabel}>Trash</div>
         </div>
       </div>
 
-        {/* Row 2 */}
+      {/* Row 2 */}
       <div style={styles.iconRow}>
         {/* Toogle */}
         <div style={styles.icon} onClick={() => console.log('Toogle clicked')}>
-          <img src="/assets/icons/Toogle.png" alt="Toogle" style={styles.iconImage} />
+          <img
+            src="/assets/icons/Toogle.png"
+            alt="Toogle"
+            style={styles.iconImage}
+          />
           <div style={styles.iconLabel}>Toogle</div>
         </div>
+
         {/* Notepad */}
-        <div style={styles.icon} onClick={() => console.log('Notepad clicked')}>
-          <img src="/assets/icons/Notepad.png" alt="Notepad" style={styles.iconImage} />
+        <div
+          style={styles.icon}
+          onClick={() => console.log('Notepad clicked')}
+        >
+          <img
+            src="/assets/icons/Notepad.png"
+            alt="Notepad"
+            style={styles.iconImage}
+          />
           <div style={styles.iconLabel}>Notepad</div>
         </div>
       </div>
 
-        {/* Row 3 */}
+      {/* Row 3 */}
       <div style={styles.iconRow}>
         {/* Task Manager */}
-        <div style={styles.icon} onClick={() => console.log('Task Manager clicked')}>
-          <img src="/assets/icons/TaskManager.png" alt="Task Manager" style={styles.iconImage} />
+        <div
+          style={styles.icon}
+          onClick={() => console.log('Task Manager clicked')}
+        >
+          <img
+            src="/assets/icons/TaskManager.png"
+            alt="Task Manager"
+            style={styles.iconImage}
+          />
           <div style={styles.iconLabel}>Task Manager</div>
         </div>
-        {/* Emails and Password */}
-        <div style={styles.icon} onClick={() => console.log('Emails & Passwords clicked')}>
-          <img src="/assets/icons/Emailsandpw.png" alt="Emails & Password" style={styles.iconImage} />
+
+        {/* Emails & Passwords */}
+        <div
+          style={styles.icon}
+          onClick={() => console.log('Emails & Passwords clicked')}
+        >
+          <img
+            src="/assets/icons/Emailsandpw.png"
+            alt="Emails & Password"
+            style={styles.iconImage}
+          />
           <div style={styles.iconLabel}>Emails & Passwords</div>
         </div>
       </div>
 
-       {/* Row 4 */}
+      {/* Row 4 */}
       <div style={styles.iconRow}>
         {/* IP Tracker */}
-        <div style={styles.icon} onClick={() => console.log('IP Tracker clicked')}>
-          <img src="/assets/icons/IP_Tracker.png" alt="IP Tracker" style={styles.iconImage} />
+        <div
+          style={styles.icon}
+          onClick={() => console.log('IP Tracker clicked')}
+        >
+          <img
+            src="/assets/icons/IP_Tracker.png"
+            alt="IP Tracker"
+            style={styles.iconImage}
+          />
           <div style={styles.iconLabel}>IP Tracker</div>
         </div>
-
       </div>
-      
+
+      {/* Email Popup */}
+      {showEmail && (
+        <div style={styles.popup}>
+          <h2>Inbox</h2>
+          <p><strong>From:</strong> user@example.com</p>
+          <p><strong>Subject:</strong> Hey IT! I need help!</p>
+          <button onClick={() => setShowEmail(false)}>Close</button>
+        </div>
+      )}
     </div>
   );
 }
@@ -79,6 +144,7 @@ const styles = {
     marginTop: '40px',
     marginLeft: '30px',
   },
+
   icon: {
     display: 'flex',
     flexDirection: 'column',
@@ -86,15 +152,41 @@ const styles = {
     cursor: 'pointer',
     width: '64px',
   },
+
   iconImage: {
     width: '72px',
     height: '72px',
   },
+
   iconLabel: {
     marginTop: '8px',
     color: 'white',
     fontSize: '14px',
     textAlign: 'center',
+  },
+
+  redDot: {
+    position: 'absolute',
+    top: 2,
+    right: 2,
+    width: 10,
+    height: 10,
+    backgroundColor: 'red',
+    borderRadius: '50%',
+    border: '1px solid white',
+    zIndex: 5,
+  },
+
+  popup: {
+    position: 'absolute',
+    top: 100,
+    left: 120,
+    width: 300,
+    padding: 20,
+    backgroundColor: 'white',
+    border: '2px solid black',
+    borderRadius: 8,
+    zIndex: 1000,
   },
 };
 
